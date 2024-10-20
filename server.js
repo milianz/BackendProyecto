c68e76073be270059d6a7967b785c493d90a6350
Ubuntu <ubuntu@ip-172-31-28-219.us-east-2.compute.internal>
2024-10-20 20:43:15 +0000

    Add: Https server

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 import dotenv from 'dotenv';
-import app from './src/app.js';
-import { connectDB } from './src/config/database.js';
+   import https from 'https';
+   import fs from 'fs';
+   import app from './src/app.js';
+   import { connectDB } from './src/config/database.js';
 
-dotenv.config();
-const PORT = process.env.PORT || 3000;
+   dotenv.config();
+   const PORT = process.env.PORT || 3000;
 
-connectDB();
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+   connectDB();
+
+   const httpsOptions = {
+     key: fs.readFileSync('/etc/ssl/inmomarket/privkey.pem'),
+     cert: fs.readFileSync('/etc/ssl/inmomarket/fullchain.pem')
+   };
+
+   https.createServer(httpsOptions, app).listen(PORT, () => {
+     console.log(`HTTPS Server running on port ${PORT}`);
+   });
