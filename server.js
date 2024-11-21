be6946b0d013edc2734595064dac2d7499d216cc
Josue Zelada <00053722@uca.edu.sv>
2024-11-20 18:25:22 -0600

    ADD: Proyecto listo en local  :rocket:

--- server.js
+++ server.js
@@ -1,19 +1,9 @@
 import dotenv from 'dotenv';
-   import https from 'https';
-   import fs from 'fs';
-   import app from './src/app.js';
-   import { connectDB } from './src/config/database.js';
+import app from './src/app.js';
+import { connectDB } from './src/config/database.js';
 
-   dotenv.config();
-   const PORT = process.env.PORT || 3000;
+dotenv.config();
+const PORT = process.env.PORT || 3000;
 
-   connectDB();
-
-   const httpsOptions = {
-     key: fs.readFileSync('/etc/ssl/inmomarket/privkey.pem'),
-     cert: fs.readFileSync('/etc/ssl/inmomarket/fullchain.pem')
-   };
-
-   https.createServer(httpsOptions, app).listen(PORT, () => {
-     console.log(`HTTPS Server running on port ${PORT}`);
-   });
+connectDB();
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
